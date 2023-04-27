0b14f98888833de7fbf670a6309babeea7c592ff
Mehul <mehulrana159@gmail.com>
2023-04-28 02:08:36 +0530

    Changes in MongoDB URI

--- data/database.js
+++ data/database.js
@@ -5,6 +5,6 @@ export const connectDB = () => {
     .connect(process.env.MONGO_URI, {
       dbName: "backendApi",
     })
-    .then(() => console.log("DataBase Connected"))
+    .then((c) => console.log(`DataBase Connected with ${c.connection.host}`))
     .catch((e) => console.log(e));
 };
